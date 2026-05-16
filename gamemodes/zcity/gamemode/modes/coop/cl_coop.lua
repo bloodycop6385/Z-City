bb173017ed7ab46b1ccbaab024de688fb0c7398f
Riggs <riggs9162@gmx.de>
2026-05-16 23:11:07 +0200

    feat(coop): render coop spectator HUD

--- gamemodes/zcity/gamemode/modes/coop/cl_coop.lua
+++ gamemodes/zcity/gamemode/modes/coop/cl_coop.lua
@@ -472,21 +472,20 @@ function MODE:RenderScreenspaceEffects()
 end
 
 function MODE:HUDPaint()
+    DrawCoopSpectatorHUD()
+
     if zb.ROUND_START + 8.5 < CurTime() then return end
 
 	if not lply:Alive() then return end
 	zb.RemoveFade()
     local fade = math.Clamp(zb.ROUND_START + 8 - CurTime(),0,1)
-	local team_ = lply:Team()
     draw.SimpleText("Homicide | CO-OP", "ZB_HomicideMediumLarge", sw * 0.5, sh * 0.1, Color(0,162,255, 255 * fade), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
     local Rolename = (lply.role and lply.role.name) or "Unknown"
-    local ColorRole = teams[0].color1
-    ColorRole.a = 255 * fade
-    draw.SimpleText("You are "..Rolename , "ZB_HomicideMediumLarge", sw * 0.5, sh * 0.5, ColorRole, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+    local ColorRole = Color(teams[0].color1.r, teams[0].color1.g, teams[0].color1.b, 255 * fade)
+    draw.SimpleText("You are " .. Rolename, "ZB_HomicideMediumLarge", sw * 0.5, sh * 0.5, ColorRole, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 
     local Objective = lply.PlayerClassName == "Gordon" and "Lead the resistance to victory!" or "Follow the Gordon!"
-    local ColorObj = teams[0].color2
-    ColorObj.a = 255 * fade
+    local ColorObj = Color(teams[0].color2.r, teams[0].color2.g, teams[0].color2.b, 255 * fade)
     draw.SimpleText( Objective, "ZB_HomicideMedium", sw * 0.5, sh * 0.9, ColorObj, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 end
 
